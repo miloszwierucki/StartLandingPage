41635d0af7f0738f54f01f6469f73d89f8cc5884
miloszwierucki <mibru2016@gmail.com>
2022-12-17 01:32:06 +0100

    Add Hamburger Menu and improve responsiveness

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-let menuBtn = document.getElementById('menu');
-console.log(menuBtn)
+const burgerBtn = document.getElementById("menu");
+const navList = document.querySelector(".nav-list");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const menuElements = document.querySelectorAll(".nav-element");
+let checked = 0;
+
+burgerBtn.addEventListener("click", () => {
+  toggleMenu();
+});
+menuElements.forEach((x) => {
+  x.addEventListener("click", () => {
+    toggleMenu();
+  });
+});
+
+const toggleMenu = () => {
+  navList.classList.toggle("active");
+  if (checked === 0) {
+    openMenu.classList.toggle("active-button-menu");
+    closeMenu.classList.toggle("active-button-menu");
+    checked != checked;
+    console.log(checked);
+  }
+};
